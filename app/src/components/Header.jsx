36e01d6848d689a54087a3a089d3c9bc68e75ca0
Maury <lynuxazura@gmail.com>
2026-06-09 09:19:16 -0600

    ft:Implement Header

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = ({ title }) => {
   return (
-    <div className="fixed bg-blue-800 w-full h-[20px]">
-      <h1>{title}</h1>
+    <div className="fixed bg-blue-800 w-full h-auto px-5 py-6">
+      <h1 className="text-white font-bold text-2xl">{title}</h1>
     </div>
   );
 };
