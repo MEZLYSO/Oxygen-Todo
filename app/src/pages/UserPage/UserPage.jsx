2761fd967cae19cbae0f98f0a0f733ec0001ba1d
Rebe HC <hernandezcruzrebeca767@gmail.com>
2026-06-15 14:10:50 -0600

    User

--- app/src/pages/UserPage/UserPage.jsx
+++ app/src/pages/UserPage/UserPage.jsx
@@ -2,6 +2,8 @@ import { Crown, Save } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { Header } from "../../components/Header.jsx";
 import { usePageUser } from "./hooks/useUserPage.js";
+import { ChevronsLeft } from "lucide-react";
+import { LogOut } from "lucide-react";
 
 export const UserPage = () => {
   const { userData, handleClickPremium } = usePageUser();
@@ -17,36 +19,43 @@ export const UserPage = () => {
     <>
       <Header
         left={
-          <button onClick={() => navigate("/home", { replace: true })}>
-            Regresar
+          <button onClick={() => navigate("/home", { replace: true })}
+          className="flex gap-2 bg-cafef duration-300 font-bold font-[Open_Sans] text-xl rounded px-5 py-2 text-white mb-3 mt-3 cursor-pointer hover:bg-cafec"
+          >
+            <ChevronsLeft />Regresar
           </button>
         }
-        center={<p className="text-white text-2xl font-bold">Cuenta</p>}
+        center={<p className="text-white text-2xl font-[Open_Sans] font-bold">Cuenta</p>}
+
+        right={
+          <button onClick={() => handleCloseSession()} 
+        className="flex gap-2 bg-azulf duration-300 font-bold font-[Open_Sans] text-xl rounded-xl px-5 py-2 text-white mb-3 mt-3 cursor-pointer hover:bg-red-700" >
+          <LogOut /> Cerrar sesion</button>
+        }
       />
-      <form className="flex flex-col">
-        <label>Username:</label>
-        <input type="text" defaultValue={userData.username} />
-        <label>Email:</label>
-        <input type="text" defaultValue={userData.email} />
-        <label>Password:</label>
-        <input type="text" defaultValue={userData.password} />
+      <form className="flex flex-col gap-2 items-center mt-15 h-screen bg-fondo">
+        <label className="py-2 px-1 font-bold font-[Open_Sans] text-xl text-cafef">Username:</label>
+        <input type="text" defaultValue={userData.username} className="border-1 text-azulf font-bold font-[Open_Sans] text-xl py-2 px-2 rounded-xl focus:outline-hidden focus:border-cafec" />
+        <label className="py-2 px-1 font-bold font-[Open_Sans] text-xl text-cafef">Email:</label>
+        <input type="text" defaultValue={userData.email} className="border-1 text-azulf font-bold font-[Open_Sans] text-xl py-2 px-2 rounded-xl focus:outline-hidden focus:border-cafec" />
+        <label className="py-2 px-1  font-bold font-[Open_Sans] text-xl text-cafef">Password:</label>
+        <input type="text" defaultValue={userData.password} className="border-1 text-azulf font-bold font-[Open_Sans] text-xl py-2 px-2 rounded-xl focus:outline-hidden focus:border-cafec" />
         <div>
-          <button className="flex gap-1 bg-green-200">
+          <button  className="flex bg-green-700 duration-300 font-bold font-[Open_Sans] text-xl rounded-xl px-5 py-2 text-white mb-3 mt-3 cursor-pointer hover:bg-cafef">
             Guardar Cambios <Save />
           </button>
         </div>
-      </form>
-      <div>
+
+        <div className="flex flex-col items-center">
         <button
           onClick={() => handleClickPremium()}
-          className={`flex gap-2 ${userData.premium == 0 ? "bg-yellow-600" : "bg-red-600"} text-white px-3 py-2 rounded focus:outline-none`}
+          className={`flex gap-2 ${userData.premium == 0 ? "bg-yellow-600" : "bg-red-600"} text-white font-[Open_Sans] px-3 py-2 mt-3 rounded-xl focus:outline-none`}
         >
           {userData.premium === 0 ? "Comprar" : "Cancelar"} Premium <Crown />
         </button>
       </div>
-      <div>
-        <button onClick={() => handleCloseSession()}>Cerrar sesion</button>
-      </div>
+      </form>
+      
     </>
   );
 };
