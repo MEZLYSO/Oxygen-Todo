131301f4c9343950a051f39d654842a6daf9bae7
Rebe HC <hernandezcruzrebeca767@gmail.com>
2026-06-15 10:10:03 -0600

    bg-button

--- app/src/components/ButtonFloat.jsx
+++ app/src/components/ButtonFloat.jsx
@@ -3,7 +3,7 @@ import { Plus } from "lucide-react";
 export const ButtonFloat = ({ handleClick }) => {
   return (
     <div className="fixed bottom-5 right-5 p-2">
-      <div onClick={handleClick} className="bg-green-300 rounded-full p-2">
+      <div onClick={handleClick} className="bg-azulf text-white rounded-full p-2 cursor-pointer hover:bg-cafef">
         <Plus className="w-10 h-10" />
       </div>
     </div>
